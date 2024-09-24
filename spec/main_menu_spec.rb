6e4f6fa7f0f5da59250f0522e1c33f96a4786afa
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-24 20:56:10 +0200

    Add test for greeting player with example greeting msg

--- spec/main_menu_spec.rb
+++ spec/main_menu_spec.rb
@@ -2,5 +2,10 @@ require "rspec"
 
 Rspec.describe MainMenu do
   describe "#open_main_menu" do
+    xit "prints a welcome message and menu using a playerIO manager" do
+      welcome_message = "Welcome to hangman"
+      expect(@player_io).to_receive(:print).with(welcome_message)
+      subject.open_main_menu
+    end
   end
 end
