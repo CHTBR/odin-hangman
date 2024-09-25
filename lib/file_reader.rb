6383b26df9e4223651f5f0086dbad3638cb3a92e
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-25 17:32:05 +0200

    Add script for reading file contents and change class to mod bc no state

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -1,3 +1,6 @@
-# Class for reading word source file
-class FileReader
+# Module for reading word source file
+module FileReader
+  def self.list_of_words
+    File.readlines("assets/google-10000-english-no-swears.txt").map { |line| line.chomp }
+  end
 end
--- spec/file_reader_spec.rb
+++ spec/file_reader_spec.rb
@@ -4,7 +4,7 @@ require_relative "../lib/file_reader"
 RSpec.describe FileReader do
   describe "#list_of_words" do
     it "returns a list of words from google-10000-english-no-swears.txt" do
-      expect(subject.list_of_words).to eql(File.readlines("google-10000-english-no-swears.txt").map { |line| line.chomp })
+      expect(FileReader.list_of_words).to eql(File.readlines("assets/google-10000-english-no-swears.txt").map { |line| line.chomp })
     end
   end
 end
