ecd9d2c2c9c8283d012277502e1c9cf40b722aec
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-25 14:39:03 +0200

    Add main test for #print

--- spec/player_io_spec.rb
+++ spec/player_io_spec.rb
@@ -3,14 +3,14 @@ require_relative "../lib/player_io"
 
 RSpec.describe PlayerIO do
   describe "#print" do
-
+    xit "prints text its given to the screen" do
+      expect(subject.print("Example text")).to output("Example text").to_stdout
+    end
   end
 
   describe "#get_option" do
-
   end
 
   describe "#set_global_option" do
-
   end
 end
