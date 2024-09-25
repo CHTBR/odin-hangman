12cb905b7a06514fe01608890743f8cd0a4ca5f2
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-25 19:20:59 +0200

    Add test for rconstructing object from json and continuing

--- spec/save_load_manager_spec.rb
+++ spec/save_load_manager_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe SaveLoadManager do
       @game_manager = GameManager.new({ player_io: PLayerIO.new,
                                         guess_evaluator: GuessEvaluator.new,
                                         file_reader: FileReader.new,
-                                        save_manager: SaveLoadManager.new })
+                                        save_manager: subject })
       @buffer = StringIO.new
       @filename = "assets/save.json"
     end
@@ -23,12 +23,29 @@ RSpec.describe SaveLoadManager do
       allow_any_instance_of(Kernel).to receive(:gets).and_return("save")
       allow(File).to receive(:open).with(filename, "w").and_yield(@buffer)
 
-      @game_manager.satrt_new_game
+      @game_manager.start_new_game
 
       expect(@buffer).to eql(@game_manager.to_json)
     end
   end
 
   describe "#load_game" do
+    before do
+      game_manager = GameManager.new({ player_io: PLayerIO.new,
+                                       guess_evaluator: GuessEvaluator.new,
+                                       file_reader: FileReader.new,
+                                       save_manager: subject })
+      @buffer = StringIO.new(game_manager.to_json)
+      @filename = "assets/save.json"
+    end
+
+    it "converts a json into a GameManager object and starts it up" do
+      allow_any_instance_of(Kernel).to receive(:gets).and_return("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+                                                                 "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+                                                                 "w", "x", "y", "z")
+      allow(File).to receive(:open).with(filename, "r").and_yield(@buffer)
+
+      expect { subject.load_game }.to output(/Sadly, your guess was incorrect. Try again./).to_stdout
+    end
   end
 end
