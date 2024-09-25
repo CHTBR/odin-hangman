de6c64a1402a34ea13502d06b77cab9aac04736b
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-25 16:26:37 +0200

    Add tests for target accessors

--- spec/guess_evaluator_spec.rb
+++ spec/guess_evaluator_spec.rb
@@ -2,6 +2,12 @@ require "rspec"
 require_relative "../lib/guess_evaluator"
 
 RSpec.describe GUessEvaluator do
+  it "has a accessors for target" do
+    guess_evaluator = subject
+    guess_evaluator.target = "target"
+    expect(guess_evaluator.target).to eql("target")
+  end
+
   describe "#evaluate_guess" do
   end
 end
