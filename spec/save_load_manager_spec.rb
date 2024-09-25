89bf39b8819b1b1999fdfb0ed5f0c806db9e9d86
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-25 18:49:17 +0200

    Add test for converting objects to json and saving them to a file

--- spec/save_load_manager_spec.rb
+++ spec/save_load_manager_spec.rb
@@ -1,12 +1,34 @@
 require "rspec"
 require_relative "../lib/save_load_manager"
 
+require "json"
+
+require_relative "../lib/game_manager"
+require_relative "../lib/player_io"
+require_relative "../lib/guess_evaluator"
+require_relative "../lib/file_reader"
+
 RSpec.describe SaveLoadManager do
   describe "#save_game" do
+    before do
+      @game_manager = GameManager.new({ player_io: PLayerIO.new,
+                                        guess_evaluator: GuessEvaluator.new,
+                                        file_reader: FileReader.new,
+                                        save_manager: SaveLoadManager.new })
+      @buffer = StringIO.new
+      @filename = "assets/save.json"
+    end
 
+    it "converts a GameManager object into json and writes it to a file" do
+      allow_any_instance_of(Kernel).to receive(:gets).and_return("save")
+      allow(File).to receive(:open).with(filename, "w").and_yield(@buffer)
+
+      @game_manager.satrt_new_game
+
+      expect(@buffer).to eql(@game_manager.to_json)
+    end
   end
 
   describe "#load_game" do
-
   end
 end
