4f749e4581b425219f2eebf1cf0f213b6bb43c54
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-25 05:47:43 +0200

    Add context for first group of tests

--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -3,9 +3,11 @@ require_relative "../lib/game_manager"
 
 RSpec.describe GameManager do
   describe "#start_new_game" do
-    xit "tries to get a list of possible words from the file reader" do
-      expect(@file_reader).to receive(:list_of_words)
-      subject.start_new_game
+    context "before initializing the game loop" do
+      xit "tries to get a list of possible words from the file reader" do
+        expect(@file_reader).to receive(:list_of_words)
+        subject.start_new_game
+      end
     end
   end
 end
