c09d1f1fa1f8772df240e3586992a74dc8b2b3a1
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-25 17:26:21 +0200

    Add test for returning contents of random word source file

--- spec/file_reader_spec.rb
+++ spec/file_reader_spec.rb
@@ -3,5 +3,8 @@ require_relative "../lib/file_reader"
 
 RSpec.describe FileReader do
   describe "#list_of_words" do
+    it "returns a list of words from google-10000-english-no-swears.txt" do
+      expect(subject.list_of_words).to eql(File.readlines("google-10000-english-no-swears.txt").map { |line| line.chomp })
+    end
   end
 end
