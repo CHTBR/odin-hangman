2ab31f7f5c8af84750f31565045599cece382e06
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-25 09:06:35 +0200

    Fix call to PlayerIO to fit it's interface

--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -107,13 +107,14 @@ RSpec.describe GameManager do
       allow(@player_io).to receive(:get_option).and_return(guesses_array)
 
       xit "removes a letter from the options array every time it's guessed" do
+        input_message = "Choose a letter:"
         allow(@guess_evaluator).to receive(:evaluate_guess).and_return(-1)
         letters_array = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
         subject.start_new_game
-        expect(@player).to have_recieved(:get_option).with(letters_array)
+        expect(@player).to have_recieved(:get_option).with({ message: input_message, options: letters_array })
         guesses_array.each do |guess|
           letters_array -= [guess]
-          expect(@player).to have_recieved(:get_option).with(letters_array)
+          expect(@player).to have_recieved(:get_option).with({ message: input_message, options: letters_array })
         end
       end
     end
