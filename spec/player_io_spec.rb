ab511b2a33b70dfb6e0341266cac1c5eea15adb3
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-25 15:30:23 +0200

    Fix string matching to regex

--- spec/player_io_spec.rb
+++ spec/player_io_spec.rb
@@ -3,8 +3,8 @@ require_relative "../lib/player_io"
 
 RSpec.describe PlayerIO do
   describe "#print" do
-    xit "prints text its given to the screen" do
-      expect(subject.print("Example text")).to output("Example text").to_stdout
+    it "prints text its given to the screen" do
+      expect { subject.print("Example text") }.to output(/Example text/).to_stdout
     end
   end
 
