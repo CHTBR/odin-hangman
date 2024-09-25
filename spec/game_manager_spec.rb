fd3d396caaa698531dd6b2b2cc58152d4f354f9d
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-25 05:54:08 +0200

    Add test for passing a word from the list to guess eval

--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe GameManager do
         expect(@file_reader).to receive(:list_of_words)
         subject.start_new_game
       end
+
+      xit "selects a word from the list and gives it to the guess evaluator" do
+        word_list = %w[this is a list of random words]
+        allow(@file_reader).to receive(:list_of_words).and_return(word_list)
+        expect(@guess_evaluator).to receive(:target=)
+        subject.start_new_game
+      end
     end
   end
 end
