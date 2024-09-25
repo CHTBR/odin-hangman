8c47f80457a7f90e9f7fd295750275c5dda3a048
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-25 16:39:44 +0200

    Add test for returning the position of the guessed letter

--- spec/guess_evaluator_spec.rb
+++ spec/guess_evaluator_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe GuessEvaluator do
     it "returns -1 when the letter isn't in the word" do
       expect(@guess_evaluator.evaluate_guess("x")).to eql(-1)
     end
+
+    it "returns the position of the letter in an array" do
+      expect(@guess_evaluator.evaluate_guess("g")).to eql([3])
+    end
   end
 end
