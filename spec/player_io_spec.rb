a09095a626d26b2f88e807a3e51642d5e5d17088
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-25 14:07:22 +0200

    Add interface method descriptions

--- spec/player_io_spec.rb
+++ spec/player_io_spec.rb
@@ -2,4 +2,15 @@ require "rspec"
 require_relative "../lib/player_io"
 
 RSpec.describe PlayerIO do
+  describe "#print" do
+
+  end
+
+  describe "#get_option" do
+
+  end
+
+  describe "#set_global_option" do
+
+  end
 end
