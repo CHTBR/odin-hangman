d70e580fe69bc7283bc7a1d2e0af8f499b8a8805
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-25 19:25:54 +0200

    Add method for saving an object into a file in json

--- lib/save_load_manager.rb
+++ lib/save_load_manager.rb
@@ -1,3 +1,10 @@
+require "json"
+
 # Class for saving and loading a game object through json
 class SaveLoadManager
+  def save_game(object)
+    File.open("assets/save.json", "w") do |file|
+      file.puts(object.to_json)
+    end
+  end
 end
