b66c7116b7464a4e546b27bf4ee3069b5f9e6d2a
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-26 16:57:32 +0200

    Add script for writing json to file

--- lib/save_load_manager.rb
+++ lib/save_load_manager.rb
@@ -1,3 +1,6 @@
 # Class for managing writing and reading saves to and from a file
 class SaveLoadManager
+  def self.save_game(object)
+    File.open("assets/save.json", "w") { |file| file.puts object.to_json }
+  end
 end
