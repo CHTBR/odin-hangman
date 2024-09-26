a7d981fd68646b8830d6ca51caeba29b7ec55686
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-26 16:52:14 +0200

    Add test for writing save data to file

--- spec/save_load_manager_spec.rb
+++ spec/save_load_manager_spec.rb
@@ -3,11 +3,18 @@ require_relative "../lib/save_load_manager"
 require_relative "game_manager_double"
 
 RSpec.describe SaveLoadManager do
+  let(:subject) { SaveLoadManager }
+
   describe "#save_game" do
-    
+    it "takes the object json representation and saves it to a file" do
+      file_double = StringIO.new
+      allow(File).to receive(:open).with("assets/save.json", "w").and_yield(file_double)
+      game_manager_double = GameManagerDouble.new
+      subject.save_game(game_manager_double)
+      expect(file_double.string.chomp).to eql(game_manager_double.json_representation)
+    end
   end
 
   describe "#load_game" do
-    
   end
 end
