2f30724797f4b175ca49fd4a32502d41f751ffef
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-26 17:10:32 +0200

    Add script for reading json and intializing object

--- lib/save_load_manager.rb
+++ lib/save_load_manager.rb
@@ -3,4 +3,8 @@ class SaveLoadManager
   def self.save_game(object)
     File.open("assets/save.json", "w") { |file| file.puts object.to_json }
   end
+
+  def self.load_game(object)
+    object.parse_json(File.read("assets/save.json"))
+  end
 end
