ddcbec854b57567aced05e48e1dc0dd606fadf7b
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-09-26 17:16:37 +0200

    Add object initiation and game kickoff script

--- main.rb
+++ main.rb
@@ -4,3 +4,12 @@ require_relative "lib/player_io"
 require_relative "lib/guess_evaluator"
 require_relative "lib/file_reader"
 require_relative "lib/save_load_manager"
+
+player_io = PlayerIO.new
+guess_evaluator = GuessEvaluator.new
+file_reader = FileReader
+save_load_manager = SaveLoadManager
+game_manager = GameManager.new({ file_reader: file_reader, guess_evaluator: guess_evaluator, player_io: player_io, save_manager: save_load_manager })
+main_menu = MainMenu.new( player_io: player_io, game_manager: game_manager, load_manager: save_load_manager )
+main_menu.open_main_menu
+
